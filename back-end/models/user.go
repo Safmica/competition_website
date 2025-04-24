041aa06d519bca5b49a86d853750a0ff76eeb820
Safmica <safmikasa@gmail.com>
2025-04-25 02:06:04 +0700

    refector(models/user): change coloumn name to id

--- back-end/models/user.go
+++ back-end/models/user.go
@@ -8,12 +8,12 @@ import (
 )
 
 type User struct {
-	ID       uuid.UUID `gorm:"type:char(36);primaryKey;column:id_user" json:"id_user"`
-	Name     string    `gorm:"type:varchar(255)" json:"name"`
-	Email    string    `gorm:"type:varchar(255);unique" json:"email"`
-	Password string    `gorm:"type:varchar(60)" json:"-"`
-	Role string `gorm:"type:enum('user', 'admin');default:'user" json:"role"`
-	CreatedAt           time.Time      `json:"-" gorm:"autoCreateTime"`
-	UpdatedAt           time.Time      `json:"-" gorm:"autoUpdateTime"`
-	DeletedAt           gorm.DeletedAt `json:"-" gorm:"index"`
+	ID        uuid.UUID      `gorm:"type:char(36);primaryKey" json:"id"`
+	Name      string         `gorm:"type:varchar(255)" json:"name"`
+	Email     string         `gorm:"type:varchar(255);unique" json:"email"`
+	Password  string         `gorm:"type:varchar(60)" json:"-"`
+	Role      string         `gorm:"type:enum('user', 'admin');default:'user" json:"role"`
+	CreatedAt time.Time      `json:"-" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"-" gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
