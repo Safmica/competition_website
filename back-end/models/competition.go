e98e299749390f9aa5305daae8624dfe5e774bd0
Safmica <safmikasa@gmail.com>
2025-04-25 03:19:49 +0700

    fix: add UpdatedAt, DeletedAt and CratedAt

--- back-end/models/competition.go
+++ back-end/models/competition.go
@@ -1,11 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Competition struct {
-	ID          string    `gorm:"type:char(36);primaryKey" json:"id"`
-	Title       string    `gorm:"type:varchar(255)" json:"title"`
-	Description string    `gorm:"type:text" json:"description"`
-	Deadline    time.Time `gorm:"type:datetime" json:"deadline"`
-	HTM         float64   `gorm:"type:float" json:"htm"`
+	ID          string         `gorm:"type:char(36);primaryKey" json:"id"`
+	Title       string         `gorm:"type:varchar(255)" json:"title"`
+	Description string         `gorm:"type:text" json:"description"`
+	Deadline    time.Time      `gorm:"type:datetime" json:"deadline"`
+	HTM         float64        `gorm:"type:float" json:"htm"`
+	CreatedAt   time.Time      `json:"-" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `json:"-" gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
 }
--- back-end/models/registration.go
+++ back-end/models/registration.go
@@ -1,15 +1,22 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Registration struct {
-	ID              uuid.UUID `gorm:"type:char(36);primaryKey" json:"id"`
-	TeamName        string    `gorm:"type:varchar(255)" json:"team_name"`
-	LeaderID        uuid.UUID `gorm:"type:char(36);column:leader_id" json:"leader_id"`
-	Member1ID       uuid.UUID `gorm:"type:char(36);column:member1_id" json:"member1_id"`
-	Member2ID       uuid.UUID `gorm:"type:char(36);column:member2_id" json:"member2_id"`
-	CompetitionID   uuid.UUID `gorm:"type:char(36);column:competition_id" json:"competition_id"`
-	CompetitionName string    `gorm:"type:varchar(255)" json:"competition_name"`
-	PaymentProff    string    `gorm:"type:varchar(255)" json:"payment_proof"`
-	PaymentStatus   bool      `gorm:"type:boolean;default:false" json:"payment_status"`
+	ID            uuid.UUID      `gorm:"type:char(36);primaryKey" json:"id"`
+	TeamName      string         `gorm:"type:varchar(255)" json:"team_name"`
+	LeaderID      uuid.UUID      `gorm:"type:char(36);column:leader_id" json:"leader_id"`
+	Member1ID     uuid.UUID      `gorm:"type:char(36);column:member1_id" json:"member1_id"`
+	Member2ID     uuid.UUID      `gorm:"type:char(36);column:member2_id" json:"member2_id"`
+	CompetitionID uuid.UUID      `gorm:"type:char(36);column:competition_id" json:"competition_id"`
+	PaymentProff  string         `gorm:"type:varchar(255)" json:"payment_proof"`
+	PaymentStatus bool           `gorm:"type:boolean;default:false" json:"payment_status"`
+	CreatedAt     time.Time      `json:"-" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time      `json:"-" gorm:"autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `json:"-" gorm:"index"`
 }
