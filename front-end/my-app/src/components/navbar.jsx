1542091146bae7e3904539c20451b5b997b01fa9
DavinVin <166722049+DavinVin@users.noreply.github.com>
2025-05-09 01:02:15 +0700

    login, navbar, register page update

--- front-end/my-app/src/components/navbar.jsx
+++ front-end/my-app/src/components/navbar.jsx
@@ -6,35 +6,35 @@ const Navbar = () => {
       <div className="bg-transparent container mx-auto flex items-center justify-between px-6 py-3">
         {/* Logo */}
         <div className="text-2xl font-bold">
-        <img src="/Logo.png" alt="KESEDZ" className="w-34"></img>
+          <img src="/Logo.png" alt="KESEDZ" className="w-34"></img>
         </div>
 
         {/* Navigation Links */}
         <div className="hidden md:flex space-x-6 items-center">
           <a
             href="/home"
-            className="hover:text-gray-300 transition-colors duration-300"
+            className="text-white hover:text-gray-300 transition-colors duration-300"
           >
             Home
           </a>
           <div className="relative group">
             <a
               href="/competition"
-              className="hover:text-gray-300 transition-colors duration-300"
+              className="text-white hover:text-gray-300 transition-colors duration-300"
             >
               Competition
             </a>
             {/* Dropdown Menu */}
-            <div className="absolute hidden group-hover:block bg-black bg-opacity-90 mt-2 rounded shadow-lg py-2">
+            <div className=" text-white absolute hidden group-hover:block bg-black bg-opacity-90 mt-2 rounded shadow-lg py-3">
               <a
                 href="/competition/1"
-                className="block px-4 py-2 hover:bg-gray-700 hover:text-gray-300"
+                className="text-white block px-4 py-3  hover:bg-gray-700 hover:text-gray-300"
               >
                 Competition 1
               </a>
               <a
                 href="/competition/2"
-                className="block px-4 py-2 hover:bg-gray-700 hover:text-gray-300"
+                className=" text-white block px-4 py-2 hover:bg-gray-700 hover:text-gray-300"
               >
                 Competition 2
               </a>
@@ -42,7 +42,7 @@ const Navbar = () => {
           </div>
           <a
             href="/about"
-            className="hover:text-gray-300 transition-colors duration-300"
+            className="text-white hover:text-gray-300 transition-colors duration-300"
           >
             About Us
           </a>
